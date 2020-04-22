191f8755c397e9b3eb7390db7514176e8fa374f4
Aman Desai <aman.desai@tomilkieway.com>
2020-04-22 13:59:51 +0530

    Debug

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ cors = require('cors')
 port = process.env.PORT || 3000
 app = express()
 app.use(cors({optionsSuccessStatus: 200}));
+app.use(express.urlencoded());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "webhook.site");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.post('/earthquake', (req, res) => {
     console.log(req.body)
