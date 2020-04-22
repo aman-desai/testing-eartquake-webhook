94613d70b298b61496887e4256bef54bb369f496
Aman Desai <aman.desai@tomilkieway.com>
2020-04-22 14:39:27 +0530

    Debug

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 express = require('express')
 cors = require('cors')
+bodyParser = require('body-parser')
 
 port = process.env.PORT || 3000
 app = express()
 app.use(cors({optionsSuccessStatus: 200}));
-app.use(express.urlencoded());
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "webhook.site");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
