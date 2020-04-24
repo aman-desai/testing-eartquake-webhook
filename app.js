d26298181bd169f148de3eb580d33dbbd5c511d0
Aman Desai <aman.desai@tomilkieway.com>
2020-04-25 00:33:49 +0530

    retry

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ bodyParser = require('body-parser')
 port = process.env.PORT || 3000
 app = express()
 app.use(cors({optionsSuccessStatus: 200}));
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "webhook.site");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,7 +14,12 @@ app.use(bodyParser.json())
 //   });
 
 app.post('/earthquake', (req, res) => {
-    console.log(req.body)
+    // var str = JSON.stringify(req.body)
+    // var st = str.split("/(\r\n|\n|\r)/gm")
+    // st.forEach(element => {
+    //     console.log(element)
+    // });
+    console.log(Object.keys(req.body))    
     res.send('OK')
 })
 
